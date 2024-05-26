3587bc5225c9910c5799d8f1a750c43316029b59
Kuingsmile <ma_shiqing@163.com>
2024-05-26 14:41:48 +0800

    :sparkles: Feature(custom): update manual source URL based on language configuration

--- src/renderer/pages/DocumentPage.vue
+++ src/renderer/pages/DocumentPage.vue
@@ -1,6 +1,6 @@
 <template>
   <webview
-    src="https://piclist.cn/app.html"
+    :src="srcUrl"
     disablewebsecurity
     allowpopups
     autosize="on"
@@ -8,6 +8,25 @@
     style="width: 100%; height: 100%;"
   />
 </template>
+
+<script lang="ts" setup>
+import { getConfig } from '@/utils/dataSender'
+import { onMounted, ref } from 'vue'
+import { II18nLanguage } from '~/universal/types/enum'
+import { configPaths } from '~/universal/utils/configPaths'
+
+const srcUrl = ref('https://piclist.cn/app.html')
+
+const updateUrl = async () => {
+  const lang = await getConfig(configPaths.settings.language) || II18nLanguage.ZH_CN
+  srcUrl.value = lang === II18nLanguage.ZH_CN ? 'https://piclist.cn/app.html' : 'https://piclist.cn/en/app.html'
+}
+
+onMounted(() => {
+  updateUrl()
+})
+
+</script>
 
 <script lang="ts">
 export default {
